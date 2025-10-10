4ea84bdec5632a0a05fb5409a77bc0b352df7879
ThiagoGuilherme71 <thiagoguilherme.barbosaa@gmail.com>
2025-10-10 19:32:49 -0300

    fix: remoção de url instável para tradução em pt-br

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -118,9 +118,6 @@
                     }
                 }
             ],
-            language: {
-                url: '//cdn.datatables.net/plug-ins/2.0.0/i18n/pt-BR.json'
-            },
             pageLength: 10,
             order: [[0, 'asc']],
             responsive: true
